88e4dd18d106c48461363ff66206c516ab1c5e32
reubz98 <reubenbotha@gmail.com>
2020-11-03 13:45:06 +0200

    Classes for Bill, Complaint, Review and Unit added. Login and Register Activities added. jmsp

---
+++ app/src/main/java/com/example/westwoodhomes/Unit.java
@@ -0,0 +1,4 @@
+package com.example.westwoodhomes;
+
+public class Unit {
+}
---
+++ app/src/main/java/com/example/westwoodhomes/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.westwoodhomes.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.westwoodhomes.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
