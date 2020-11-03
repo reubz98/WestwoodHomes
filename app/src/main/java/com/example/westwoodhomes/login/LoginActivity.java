0eccacc2c52ccb1081b08e99ecedbd929955a9ec
Matcbox422 <marchonda62@gmail.com>
2020-11-03 14:24:56 +0200

    Login design

--- app/src/main/java/com/example/westwoodhomes/login/LoginActivity.java
+++ app/src/main/java/com/example/westwoodhomes/login/LoginActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.westwoodhomes.R;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
